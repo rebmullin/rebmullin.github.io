34b6081a2d0f7499abaf8751390c0bf056f6ea82
Rebecca <rebjmullin@gmail.com>
2019-01-30 22:00:34 -0500

    updating

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>My Portfolio</p>
+
+          <a href="https://wwe-rockstars.herokuapp.com/">link 1</a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
